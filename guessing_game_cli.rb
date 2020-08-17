572b577bb75bf4e17e3fd81926d180043f77a918
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-17 21:48:46 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   guess = gets
   puts number
   puts guess
-  if number == guess.to_int
+  if number == guess.to_i
     puts "You guessed the correct number!"
   elsif guess == "exit"
     puts "Goodbye!"
